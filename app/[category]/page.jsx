55ba3d8ee612a2c0579b1ed718bc1f7d782738a0
Chan Troi Moi <tonglanhchua@gmail.com>
2024-09-16 01:16:37 +0000

    update page.jsx

--- app/[category]/page.jsx
+++ app/[category]/page.jsx
@@ -4,37 +4,23 @@ import Nav from "../Nav";
 
 async function Category({ params }) {
   const { category } = params;
-  const infoText = `Danh sách phim ${category}`;
   const currentPage = `/${category}`;
-  try {
-    const res = await fetch(`https://phimapi.com/api/danh-sach/${category}`);
-    if (!res.ok) {
-      throw new Error("Network response was not ok");
-    }
-    const content = await res.json();
-    const movies = content.data.items;
-
-    return (
-      <>
-        <Nav currentPage={currentPage} />
-        <Container infoText={infoText}>
-          {movies.map((movie) => (
-            <Card key={movie._id} movie={movie} />
-          ))}
-        </Container>
-      </>
-    );
-  } catch (error) {
-    console.error("Error fetching data:", error);
-    return (
-      <>
-        <Nav currentPage={currentPage} />
-        <Container infoText={infoText}>
-          <p>Error fetching data.</p>
-        </Container>
-      </>
-    );
+  const res = await fetch(`https://phimapi.com/v1/api/danh-sach/${category}`);
+  if (!res.ok) {
+    throw new Error("Network response was not ok");
   }
+  const content = await res.json();
+  const movies = content.data.items;
+  const infoText = `Danh sách ${content.data.titlePage}`;
+  return (
+    <>
+      <Nav currentPage={currentPage} />
+      <Container infoText={infoText}>
+        {movies.map((movie) => (
+          <Card key={movie._id} movie={movie} />
+        ))}
+      </Container>
+    </>
+  );
 }
-
 export default Category;
