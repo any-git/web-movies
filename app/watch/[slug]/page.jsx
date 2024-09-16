9dde7e838f26b2acf11b6d178c290b4b2ca8c93e
Chan Troi Moi <tonglanhchua@gmail.com>
2024-09-16 12:37:02 +0000

    update page.jsx

--- app/watch/[slug]/page.jsx
+++ app/watch/[slug]/page.jsx
@@ -10,7 +10,7 @@ export async function generateMetadata({ params }) {
     return {
       title: movie.name,
       description: movie.content,
-      image: movie.thumb_url,
+      images: [movie.thumb_url],
     };
   }
 }
