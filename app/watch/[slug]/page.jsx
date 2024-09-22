60d25f6572634016bf8ed42e33d82bced061b931
Chan Troi Moi <tonglanhchua@gmail.com>
2024-09-22 07:11:15 +0000

    update page.jsx and page.jsx

--- app/watch/[slug]/page.jsx
+++ app/watch/[slug]/page.jsx
@@ -2,30 +2,37 @@ import React from "react";
 import Movie from "@/components/Movie";
 import Nav from "@/components/Nav";
 
-export async function generateMetadata({ params }) {
-  const { slug } = params;
-  const res = await fetch(`https://phimapi.com/phim/${slug}`);
-  if (res.ok) {
-    const data = await res.json();
-    const movie = data.movie;
-    return {
-      title: movie.name,
-      description: movie.content,
-      images: [movie.thumb_url],
-    };
-  }
-}
+const movieCache = {};
 
-async function MovieDetail({ params }) {
-  const { slug } = params;
+async function getMovieContent(slug) {
+  if (movieCache[slug]) {
+    return movieCache[slug];
+  }
   const res = await fetch(`https://phimapi.com/phim/${slug}`);
   if (!res.ok) {
     throw new Error("Network response was not ok");
   }
   const data = await res.json();
+  movieCache[slug] = data; // Lưu vào cache
+  return data;
+}
+
+export async function generateMetadata({ params }) {
+  const { slug } = params;
+  const data = await getMovieContent(slug);
+  const movie = data.movie;
+  return {
+    title: movie.name,
+    description: movie.content,
+    images: [movie.thumb_url],
+  };
+}
+
+async function MovieDetail({ params }) {
+  const { slug } = params;
+  const data = await getMovieContent(slug);
   const movie = data.movie;
   const episodes = data.episodes;
-
   return (
     <>
       <Nav currentPage={`/watch/${slug}`} />
