15c6bc1a7cab165603cb3578d3ed3b5e88360bc0
vscode <tonglanhchua@gmail.com>
2024-09-25 05:15:52 +0000

    build(deps): update Nav.jsx, package-lock.json and package.json

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,5 +1,6 @@
 "use client";
 import { useState } from "react";
+import { motion } from "framer-motion";
 
 export default function Nav({ currentPage }) {
   const [searchContent, setSearch] = useState("");
@@ -14,9 +15,9 @@ export default function Nav({ currentPage }) {
 
   return (
     <>
-      <nav className="flex justify-between items-center h-16 bg-white text-black sticky top-0 shadow-sm font-mono w-screen z-50 sticky">
-        <div className="px-2 cursor-pointer">
-          <button
+      <motion.nav className="flex justify-between items-center h-16 bg-white text-black sticky top-0 shadow-sm font-mono w-screen z-50 sticky">
+        <motion.div className="px-2 cursor-pointer">
+          <motion.button
             className="text-2xl font-bold"
             onClick={() => setOpenMenu(!openMenu)}
           >
@@ -25,30 +26,30 @@ export default function Nav({ currentPage }) {
             ) : (
               <i className="material-icons">close</i>
             )}
-          </button>
-        </div>
-        <div className="px-2 py-2 border border-gray-300 rounded-md flex">
-          <input
+          </motion.button>
+        </motion.div>
+        <motion.div className="px-2 py-2 border border-gray-300 rounded-md flex">
+          <motion.input
             type="text"
             className="border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
             value={searchContent}
             onChange={(e) => setSearch(e.target.value)}
           />
-          <a
+          <motion.a
             href={"/?q=" + encodeURIComponent(searchContent)}
             className="ml-2 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
           >
             <i className="material-icons">search</i>
-          </a>
-        </div>
-      </nav>
+          </motion.a>
+        </motion.div>
+      </motion.nav>
       {openMenu && (
-        <div className="fixed inset-0 bg-slate-300 bg-opacity-50 z-10">
-          <div className="relative left-0 bottom-0 w-1/2 bg-white shadow-lg">
-            <ul className="relative flex flex-col m-2 p-4 gap-4 mt-16">
+        <motion.div className="fixed inset-0 bg-slate-300 bg-opacity-50 z-10">
+          <motion.div className="relative left-0 bottom-0 w-1/2 bg-white shadow-lg">
+            <motion.ul className="relative flex flex-col m-2 p-4 gap-4 mt-16">
               {categories.map((category) => (
-                <li key={category.name}>
-                  <a
+                <motion.li key={category.name}>
+                  <motion.a
                     href={category.link}
                     className={`block w-full font-sans overflow-hidden hover:underline hover:cursor-pointer hover:bg-slate-600 hover:text-white px-4 py-2 ${
                       currentPage === category.link
@@ -57,12 +58,12 @@ export default function Nav({ currentPage }) {
                     }`}
                   >
                     {category.name}
-                  </a>
-                </li>
+                  </motion.a>
+                </motion.li>
               ))}
-            </ul>
-          </div>
-        </div>
+            </motion.ul>
+          </motion.div>
+        </motion.div>
       )}
     </>
   );
